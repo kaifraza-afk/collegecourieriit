86b46464d0a9a984ad719b279e2833da8f92f70d
kaifraza-afk <mdkraza@iitbhilai.ac.in>
2025-04-10 16:59:34 +0530

    I am uploading html ,css and js files .
    index.html, styles.css, and images

---
+++ script.js
@@ -0,0 +1,4 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const navLinks = document.querySelectorAll('.nav-menu a');
+const faqItems = document.querySelectorAll('.faq-item');
